29314541e71aebe77c85bbcf7e60f1c275b77310
Jameson <48740182+JamesonWelch@users.noreply.github.com>
2019-06-14 14:11:50 -0700

    Update secret_number.py

--- secret_number.py
+++ secret_number.py
@@ -1,9 +1,11 @@
 from random import randint
 
 tries = str(input('Do you want unlimited tries? Ender \'y\' or \'n\': '))
+tries = tries.lower()
 
 game = 1
 secret_num = randint(1,100)
+attempts = []
 
 if tries == 'y':
     while game == 1:
@@ -18,6 +20,7 @@ if tries == 'y':
         elif guess == secret_num:
             print(guess, 'is the correct answer')
             game = 0
+        attempts.append(guess)
 
 elif tries == 'n':
     usr_tries = randint(1,10)
@@ -34,3 +37,12 @@ elif tries == 'n':
             print(guess, 'is bigger than the secret number')
         elif guess == secret_num:
             print(guess, 'is the correct answer')
+            attempts.append(guess)
+            break
+        attempts.append(guess)
+
+print('Number of tries =', len(attempts))
+print('Max number you guessed =', max(attempts))
+print('Min number you guessed =', min(attempts))
+print('Summation of numbers =', sum(attempts)) 
+print('The average of numbers = {}'.format(round(sum(attempts)/len(attempts)),2))
