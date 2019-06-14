5f17dd85777ec374750c9f87c8d9247230f6a756
Jameson <48740182+JamesonWelch@users.noreply.github.com>
2019-06-14 13:26:23 -0700

    Create secret_number.py

---
+++ secret_number.py
@@ -0,0 +1,34 @@
+from random import randint
+
+tries = str(input('Do you want unlimited tries? Ender \'y\' or \'n\': '))
+
+game = 1
+secret_num = randint(1,100)
+
+if tries == 'y':
+    while game == 1:
+        guess = int(input('Enter a number in the range of 1-100: '))
+        if guess < 1 or guess > 100:
+            print('Invalid input! Must enter number within range of 1-100')
+            quit(0)
+        elif guess < secret_num:
+            print(guess, 'is less than the secret number')
+        elif guess > secret_num:
+            print(guess, 'is bigger than the secret number')
+        elif guess == secret_num:
+            print(guess, 'is the correct answer')
+            game = 0
+
+elif tries == 'n':
+    usr_tries = randint(1,10)
+    for i in range(usr_tries):
+        guess = int(input('Enter a number in the range of 1-100: '))
+        if guess < 1 or guess > 100:
+            print('Invalid input! Must enter number within range of 1-100')
+            quit(0)
+        elif guess < secret_num:
+            print(guess, 'is less than the secret number')
+        elif guess > secret_num:
+            print(guess, 'is bigger than the secret number')
+        elif guess == secret_num:
+            print(guess, 'is the correct answer')
