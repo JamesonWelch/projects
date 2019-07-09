5e9131593c79d4cfebf89f34753eec747824df6c
Jameson <48740182+JamesonWelch@users.noreply.github.com>
2019-07-09 08:12:34 -0700

    Continued function writing

--- test_unitt_lab.py
+++ test_unitt_lab.py
@@ -12,28 +12,43 @@ class TestPythonMethods(unittest.TestCase):
         self.assertTrue(x, bin(456))
 
     def test_bool(self):
-        pass
+        x = True
+        y = False
+        self.assertTrue(bool(x))
+        self.assertFalse(bool(y))
 
     def test_callable(self):
-        pass
+        def foo():
+            return 5
+
+        x = foo
+        nums = 6 + 7
+        self.assertTrue(callable(x))
+        self.assertFalse(callable(nums))
 
     def test_chr(self):
-        pass
+        self.assertEqual(chr(457392), "\U0006fab0")
 
     def test_dict(self):
-        pass
+        nums = dict(x=9, y=10)
+        nums_type = type(nums)
+        self.assertEqual(type(nums), nums_type)
 
     def test_divmod(self):
-        pass
+        nums = divmod(5, 2)
+        self.assertEqual(nums, (2, 1))
 
     def test_enumerate(self):
-        pass
+        stuff = [12, 45]
+        self.assertEqual(list(enumerate(stuff)), [(0, 12), (1, 45)])
 
     def test_float(self):
-        pass
+        n = "45"
+        self.assertEqual(float(n), 45.0)
 
     def test_hex(self):
-        pass
+        x = hex(12648430)
+        self.assertEqual(x, "0xc0ffee")
 
     def test_int(self):
         pass
