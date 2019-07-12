0d5e011038a96ef94c3a07f2e8448e870f7826be
Jameson <48740182+JamesonWelch@users.noreply.github.com>
2019-07-12 14:44:57 -0700

    Add files via upload
    Fixed Insufficient Funds error and updated time to include milliseconds.

--- atm.py
+++ atm.py
@@ -1,6 +1,4 @@
-import time
-
-current_time = time.strftime(r"%d.%m.%Y %H:%M:%S", time.localtime())
+import datetime
 
 
 class atm:
@@ -11,55 +9,57 @@ class atm:
         self.transactions = dict()
         self.transactions["Deposits"] = {}
         self.transactions["Withdrawals"] = {}
+        if len(str(self.pin)) != 4:
+            raise ValueError("Pin Number Too Long. Must be 4 digits.")
 
     def deposit(self, amt):
-        if amt <= 1000 and amt >= 0:
+        """ Deposits money into account up to $1000 """
+        current_time = str(datetime.datetime.now())
+        if amt <= 1000 and amt > 0:
             self.balance += amt
             self.transactions["Deposits"].update({current_time: (+amt)})
         else:
             raise ValueError("Invalid Amount.")
 
     def withdrawal(self, amt):
+        """ Withdrawals money from account up to $500 """
+        current_time = str(datetime.datetime.now())
         if amt > 500 or amt < 0:
             raise ValueError("Invalid Amount")
         elif amt <= 500 and amt > 0 and amt <= self.balance:
             self.balance -= amt
             self.transactions["Withdrawals"].update({current_time: (-amt)})
         elif amt > self.balance:
-            return "Insufficient Funds"
+            print("Insufficient Funds")
 
     def check_balance(self):
         return self.balance
 
     def get_transactions(self):
-        return self.transactions
+        for k, v in self.transactions.items():
+            print(k, v)
 
     def get_deposits(self):
         for k, v in self.transactions["Deposits"].items():
-            return "Deposit", k, v
+            print("Deposit", k, v)
 
     def get_withdrawals(self):
         for k, v in self.transactions["Withdrawals"].items():
-            return "Withdrawal", k, v
+            print("Withdrawal", k, v)
 
     def get_name(self):
-        return self.name
+        print(self.name)
 
     def get_pin(self):
-        return self.pin
-
-    def init(self):
-        self.usr = str(input("What's your name? "))
-        self.usr_pin = int(input("What's your pin number? "))
-        # if self.usr
+        print(self.pin)
 
 
 jameson = atm("Jameson", "1234")
-jameson.deposit(500)
+jameson.deposit(1000)
 print(jameson.check_balance())
 jameson.withdrawal(500)
 print(jameson.check_balance())
-jameson.withdrawal(400)
+jameson.deposit(400)
 print(jameson.check_balance())
 jameson.withdrawal(450)
 print(jameson.check_balance())
@@ -68,3 +68,9 @@ print(jameson.check_balance())
 print(jameson.get_transactions())
 print(jameson.get_deposits())
 print(jameson.get_withdrawals())
+
+trish = atm("Trish", "2345")
+trish.deposit(1000)
+
+print(trish.check_balance())
+print(trish.balance)
